b073794be6f71d3fe6261fd200ec751971800500
Lavanya-1019 <vtu26565@veltech.edu.in>
2026-02-24 09:46:22 +0530

    Time: 1 ms (40.32%) | Memory: 47 MB (16.62%) - LeetSync

---
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
@@ -0,0 +1,43 @@
+import java.util.Stack;
+
+// TreeNode class (non-public so it can be in the same file)
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {}
+
+    TreeNode(int val) { 
+        this.val = val; 
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution {
+
+    public int kthSmallest(TreeNode root, int k) {
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode current = root;
+
+        while (current != null || !stack.isEmpty()) {
+            while (current != null) {
+                stack.push(current);
+                current = current.left;
+            }
+
+            current = stack.pop();
+            k--;
+            if (k == 0) return current.val;
+
+            current = current.right;
+        }
+
+        return -1; // Will never reach here if k is valid
+    }
+}
