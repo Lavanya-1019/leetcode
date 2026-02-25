6cb33f9a5e470e6509883f3ec79f70e0e961544b
Lavanya-1019 <vtu26565@veltech.edu.in>
2026-02-25 10:19:39 +0530

    Time: 8 ms (93.42%) | Memory: 47.3 MB (47.93%) - LeetSync

---
+++ 2502-sort-the-people/sort-the-people.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+
+    static class Person {
+        String name;
+        int height;
+
+        Person(String name, int height) {
+            this.name = name;
+            this.height = height;
+        }
+    }
+
+    public String[] sortPeople(String[] names, int[] heights) {
+        int n = names.length;
+        Person[] people = new Person[n];
+
+        for (int i = 0; i < n; i++) {
+            people[i] = new Person(names[i], heights[i]);
+        }
+
+        Arrays.sort(people, (a, b) -> b.height - a.height);
+
+        String[] result = new String[n];
+        for (int i = 0; i < n; i++) {
+            result[i] = people[i].name;
+        }
+
+        return result;
+    }
+}
