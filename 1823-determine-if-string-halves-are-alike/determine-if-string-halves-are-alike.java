5bcee3ccecdac8908df57486585829007041d0e6
Lavanya-1019 <vtu26565@veltech.edu.in>
2026-03-04 10:20:46 +0530

    Time: 3 ms (91.52%) | Memory: 43 MB (79.26%) - LeetSync

---
+++ 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean halvesAreAlike(String s) {
+        int n = s.length();
+        int count1 = 0;
+        int count2 = 0;
+        
+        for (int i = 0; i < n / 2; i++) {
+            if (isVowel(s.charAt(i))) {
+                count1++;
+            }
+        }
+        
+        for (int i = n / 2; i < n; i++) {
+            if (isVowel(s.charAt(i))) {
+                count2++;
+            }
+        }
+        
+        return count1 == count2;
+    }
+    
+    private boolean isVowel(char ch) {
+        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+               ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U';
+    }
+}
