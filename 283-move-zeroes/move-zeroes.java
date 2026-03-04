52d8d4a3a92d76a8dbeaa85f440cfc77670a5bd0
Lavanya-1019 <vtu26565@veltech.edu.in>
2026-03-04 10:43:40 +0530

    Time: 1 ms (99.77%) | Memory: 47.7 MB (76.57%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int n = nums.length;
+        int insertPos = 0; 
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != 0) {
+                nums[insertPos] = nums[i];
+                insertPos++;
+            }
+        }
+        
+        while (insertPos < n) {
+            nums[insertPos] = 0;
+            insertPos++;
+        }
+    }
+}
